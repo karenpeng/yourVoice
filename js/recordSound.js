ea587c994ccc774895d4d148625774f6babb5861
Karen <karenpenglabs@gmail.com>
2014-07-02 19:52:45 -0400

    give up

--- js/recordSound.js
+++ js/recordSound.js
@@ -1,27 +1,56 @@
-var decodeBuffer = function ( buf ){
+var decodeBuffer = function (buf) {
 
-	var audioContext = window.getAudioContext();
-	var newBuffer = audioContext.createBuffer(2, buf[0].length, audioContext.sampleRate);
-	for (var channelNum = 0; channelNum < newBuffer.numberOfChannels; channelNum++){
-      var channel = newBuffer.getChannelData(channelNum);
-      channel.set(buf[channelNum]);
-    }
-    //window.setBuffer(newBuffer);
+  var audioContext = window.getAudioContext();
+  var newBuffer = audioContext.createBuffer(2, buf[0].length, audioContext.sampleRate);
+  for (var channelNum = 0; channelNum < newBuffer.numberOfChannels; channelNum++) {
+    var channel = newBuffer.getChannelData(channelNum);
+    channel.set(buf[channelNum]);
+  }
+  //window.setBuffer(newBuffer);
 };
 
-var setBuffer = function(buf){
-
+var setBuffer = function (buf) {
 
 };
 
-function init(){
+function init() {
 
-	//connect to mic
-	// create new fft
-	//create new Recorder and mic as the augement
+  //pre record
+  //show something
+
+  //recoding
+  //generate the creature
+  //fft waveform
+
+  //done recorde
+  //store the wave
+
+  // when click
+  // fft wave in real time, not the entire waveform?
+
+  //like a bottle wh
+
+  //connect to mic
+  // create new fft
+  //create new Recorder and mic as the augement
 
 }
 
-//fft waveform
-//the entire buffer wave form
-//or the peakform? :D
+function update() {
+
+  //ich stores your voice
+
+  //and then what?
+  //nodoby knows!
+
+  //where's your bravery??
+  //Show me!
+  //Fear? what stops you???
+
+  //store your voice and then you could DJ with it.
+
+  //How?
+
+  //the entire buffer wave form
+  //or the peakform? :D
+}
