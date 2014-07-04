8743cbed434b403c45fc182054902cdd332a48f7
Karen <karenpenglabs@gmail.com>
2014-07-03 20:47:35 -0400

    have new idea :)

--- js/recordSound.js
+++ js/recordSound.js
@@ -13,44 +13,8 @@ var setBuffer = function (buf) {
 
 };
 
-function init() {
+//record it
+//process it
+//play it
 
-  //pre record
-  //show something
-
-  //recoding
-  //generate the creature
-  //fft waveform
-
-  //done recorde
-  //store the wave
-
-  // when click
-  // fft wave in real time, not the entire waveform?
-
-  //like a bottle wh
-
-  //connect to mic
-  // create new fft
-  //create new Recorder and mic as the augement
-
-}
-
-function update() {
-
-  //ich stores your voice
-
-  //and then what?
-  //nodoby knows!
-
-  //where's your bravery??
-  //Show me!
-  //Fear? what stops you???
-
-  //store your voice and then you could DJ with it.
-
-  //How?
-
-  //the entire buffer wave form
-  //or the peakform? :D
-}
+//:)
